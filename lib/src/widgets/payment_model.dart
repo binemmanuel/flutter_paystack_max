5da15caefa285535f2e9cece7460f79484fe9805
Bin Emmanuel <53215111+binemmanuel@users.noreply.github.com>
2024-05-21 19:37:00 +0100

    Removed unused import

--- lib/src/widgets/payment_model.dart
+++ lib/src/widgets/payment_model.dart
@@ -3,7 +3,6 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import 'package:logger/logger.dart';
 
 import '../models/models.dart';
 
