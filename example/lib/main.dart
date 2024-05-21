3e40603a19feb9beb0349e01a2c02e8b9afb6f56
Bin Emmanuel <53215111+binemmanuel@users.noreply.github.com>
2024-05-21 18:44:19 +0100

    Removed unused import

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_paystack_max/flutter_paystack_max.dart';
-import 'package:logger/logger.dart';
 
 void main() {
   runApp(const MyApp());
